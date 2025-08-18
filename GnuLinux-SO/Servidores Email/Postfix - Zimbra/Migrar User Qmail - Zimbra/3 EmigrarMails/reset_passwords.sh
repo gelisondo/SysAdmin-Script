123a6545972908da675c0759d51cf813420f1217
Guillermo Elisondo <1world2geeks@gmail.com>
2025-08-18 10:27:55 -0300

    agregamos archivo para cambiar passworda a uno standar

---
+++ GnuLinux-SO/Servidores Email/Postfix - Zimbra/Migrar User Qmail - Zimbra/3 EmigrarMails/reset_passwords.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+zmprov ma usuario1@tecnicos.lan userPassword '{crypt}$6$i5SAhAvr$5ezei7y86T3gXn70p3ATC5H1Aqj55ZCcmfQWFGjXir21/nQASrQpvtDyqL2z924Hr53Uefn3IA0BFlSTkTXnT1'
+zmprov ma usuario2@tecnicos.lan userPassword '{crypt}$6$i5SAhAvr$5ezei7y86T3gXn70p3ATC5H1Aqj55ZCcmfQWFGjXir21/nQASrQpvtDyqL2z924Hr53Uefn3IA0BFlSTkTXnT1'
